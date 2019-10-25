e7b219380c7df32b2f140d2710cb84f1ae5f75c5
Isaac <hawkins.isaac@gmail.com>
2019-10-25 14:01:42 -0500

    adds date as a string

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+const db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'scream body',
+      createAt: '2019-10-25T18:55:05.380Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
